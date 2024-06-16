31ac67ae338e4fad0c77c3f0d19f4ac7882745c8
NHnew nabi <nbihuseynov1@gmail.com>
2024-06-16 10:34:56 +0000

    changesHeader

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-    </>
+    </BrowserRouter>
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import '../sass/Header.sass';
+import { Link } from "react-router-dom";
+import { IoLanguageOutline } from "react-icons/io5";
+import { FaPhone } from "react-icons/fa";
+
+
 
 const Header = () => {
     return (
@@ -8,7 +13,30 @@ const Header = () => {
                 <div className="container">
                     <div className="row">
                         <div className="col-md-12">
-                            
+                            <div className='d-flex justify-content-between align-items-center'>
+                                <div className='leftHeader d-flex align-items-center'>
+                                    <div className="logo fs-1">
+                                        LOGO
+                                    </div>
+                                    <div className="menu">
+                                        <Link className='link' to="/home">Ana Səhifə</Link>
+                                        <Link className='link' to="/about">Haqqımızda</Link>
+                                        <Link className='link' to="/services">Fəaliyyət Sahələri</Link>
+                                        <Link className='link' to="/hr">İnsan Resursları</Link>
+                                        <Link className='link' to="/news">Xəbərlər</Link>
+                                    </div>
+                                </div>
+                                <div className='rightHeader d-flex align-items-center'>
+                                    <div className="lang d-flex align-items-center">
+                                        <div className='m-2 fs-5'>EN</div>
+                                        <IoLanguageOutline className='fs-4' />
+                                    </div>
+                                    <div className="contact">
+                                        <Link className='link' to="/contact">Əlaqə</Link>
+                                        <FaPhone />
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
