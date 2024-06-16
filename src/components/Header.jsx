519ee991e4b90c15dfd084473c0263c12c480fe2
NHnew nabi <nbihuseynov1@gmail.com>
2024-06-16 08:21:32 +0000

    ch

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../sass/Header.sass';
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </>
+    );
+};
+
+export default Header;
