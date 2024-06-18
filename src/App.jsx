4042f9a88bd95833446c1cd85cd5c2e51bf1dea5
NHnew nabi <nbihuseynov1@gmail.com>
2024-06-18 13:40:38 +0000

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Header from './components/Header';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from './pages/About';
+import Services from './pages/Services';
+import HR from './pages/HR';
+import News from './pages/News';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
+      <Routes>
+        <Route path='/about' element={<About />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/hr' element={<HR />} />
+        <Route path='/news' element={<News />} />
+      </Routes>
     </BrowserRouter>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../sass/Header.sass';
+import Logo from '../assets/himal-logo-white.png';
 import { Link } from "react-router-dom";
 import { IoLanguageOutline } from "react-icons/io5";
-import { FaPhone } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
+import { VscMenu } from "react-icons/vsc";
+import { IoMdClose } from "react-icons/io";
+import { HiMiniArrowRight } from "react-icons/hi2";
+
+
+
 
 
 
 const Header = () => {
+
+    const [toggleMenu, setToggleMenu] = useState(false);
+
+    const handleMenuToggle = () => {
+        setToggleMenu(!toggleMenu);
+    };
+
     return (
         <>
             <header>
@@ -15,25 +29,75 @@ const Header = () => {
                         <div className="col-md-12">
                             <div className='d-flex justify-content-between align-items-center'>
                                 <div className='leftHeader d-flex align-items-center'>
-                                    <div className="logo fs-1">
-                                        LOGO
+                                    <div className="logo">
+                                        <Link to="/" className='link'>
+                                            <div className="imgbox">
+                                                <img className='logoImg' src={Logo} alt="" />
+                                            </div>
+                                        </Link>
                                     </div>
-                                    <div className="menu">
-                                        <Link className='link' to="/home">Ana Səhifə</Link>
-                                        <Link className='link' to="/about">Haqqımızda</Link>
-                                        <Link className='link' to="/services">Fəaliyyət Sahələri</Link>
-                                        <Link className='link' to="/hr">İnsan Resursları</Link>
-                                        <Link className='link' to="/news">Xəbərlər</Link>
+                                    <div className={`menu d-flex ${toggleMenu ? 'active' : ''}`}>
+                                        <Link className='link d-flex align-items-center' to="/">
+                                            <div>
+                                                Ana Səhifə
+                                            </div>
+                                            <div className='menuRightArrow'>
+                                                <HiMiniArrowRight />
+                                            </div>
+                                        </Link>
+                                        <Link className='link d-flex align-items-center' to="/about">
+                                            <div>
+                                                Haqqımızda
+                                            </div>
+                                            <div className='menuRightArrow'>
+                                                <HiMiniArrowRight />
+                                            </div>
+                                        </Link>
+                                        <Link className='link d-flex align-items-center' to="/services">
+                                            <div>
+                                                Fəaliyyət Sahələri
+                                            </div>
+                                            <div className='menuRightArrow'>
+                                                <HiMiniArrowRight />
+                                            </div>
+                                        </Link>
+                                        <Link className='link d-flex align-items-center' to="/hr">
+                                            <div>
+                                                İnsan Resursları
+                                            </div>
+                                            <div className='menuRightArrow'>
+                                                <HiMiniArrowRight />
+                                            </div>
+                                        </Link>
+                                        <Link className='link d-flex align-items-center' to="/news">
+                                            <div>
+                                                Xəbərlər
+                                            </div>
+                                            <div className='menuRightArrow'>
+                                                <HiMiniArrowRight />
+                                            </div>
+                                        </Link>
+                                        <div className="lang d-flex align-items-center">
+                                            <div className='language m-2 fs-6'>EN</div>
+                                            <IoLanguageOutline className='fs-4' />
+                                        </div>
+                                        <div className="contact d-flex align-items-center">
+                                            <Link className='link me-2' to="/contact">Əlaqə</Link>
+                                            <FaPhoneAlt />
+                                        </div>
                                     </div>
                                 </div>
                                 <div className='rightHeader d-flex align-items-center'>
                                     <div className="lang d-flex align-items-center">
-                                        <div className='m-2 fs-5'>EN</div>
+                                        <div className='m-2 fs-6'>EN</div>
                                         <IoLanguageOutline className='fs-4' />
                                     </div>
-                                    <div className="contact">
-                                        <Link className='link' to="/contact">Əlaqə</Link>
-                                        <FaPhone />
+                                    <div className="contact d-flex align-items-center">
+                                        <Link className='link me-2' to="/contact">Əlaqə</Link>
+                                        <FaPhoneAlt />
+                                    </div>
+                                    <div className="hamburgerMenuIcon" onClick={handleMenuToggle}>
+                                        {toggleMenu ? <IoMdClose /> : <VscMenu />}
                                     </div>
                                 </div>
                             </div>
