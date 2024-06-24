982adf5edd7b0bd552883771d67c7e0764c05edf
NHnew nabi <nbihuseynov1@gmail.com>
2024-06-24 13:08:58 +0000

    ch

---
+++ src/components/Section4.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import '../sass/Section4.sass';
+
+const Section4 = () => {
+    return (
+        <>
+            <section id='section4'>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="textbox">
+                                <h6>Partners</h6>
+                                <p>Whether designing new residences or transforming existing buildings, we take a human-centered.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row row-cols-md-4 row-cols-2">
+                        <div className="col-md-3">
+                            <div className="box">LOGO</div>
+                        </div>
+                        <div className="col-md-3">
+                            <div className="box">LOGO</div>
+                        </div>
+                        <div className="col-md-3">
+                            <div className="box">LOGO</div>
+                        </div>
+                        <div className="col-md-3">
+                            <div className="box">LOGO</div>
+                        </div>
+                        <div className="col-md-3">
+                            <div className="box">LOGO</div>
+                        </div>
+                        <div className="col-md-3">
+                            <div className="box">LOGO</div>
+                        </div>
+                        <div className="col-md-3">
+                            <div className="box">LOGO</div>
+                        </div>
+                        <div className="col-md-3">
+                            <div className="box">LOGO</div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default Section4;
